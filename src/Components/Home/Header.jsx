a05226d22e184145fac1770551949c40673d75a6
cethread <threadgillcheyenne@gmail.com>
2023-11-07 12:54:55 -0500

    revisions

--- src/Components/Home/Header.jsx
+++ src/Components/Home/Header.jsx
@@ -1,23 +1,28 @@
-import React from "react";
-import PropTypes from "prop-types";
-import Carousel from "react-bootstrap/Carousel";
-import { Button } from "react-bootstrap";
-import Image from "react-bootstrap/Image";
-import headerImgs from "../../data/headerImgs";
-import sliderlogo from "../../images/Home/header/slider-logo.png.webp";
-import MainNav from "../Global/MainNav";
-import {Row} from 'react-bootstrap';
+import React from 'react';
+import PropTypes from 'prop-types';
+import Carousel from 'react-bootstrap/Carousel';
+import {Button} from 'react-bootstrap';
+import Image from 'react-bootstrap/Image';
+import headerImgs from '../../data/headerImgs';
+import sliderlogo from '../../images/Home/header/slider-logo.png.webp';
+import MainNav from '../Global/MainNav';
+import {Row, Col} from 'react-bootstrap';
 
-const Header = ({ cartLength, cart }) => {
+const Header = ({cartLength, cart}) => {
   Header.propTypes = {
     cart: PropTypes.arrayOf.isRequired,
     cartLength: PropTypes.number.isRequired,
   };
 
   return (
-    <section className="home_header bg-dark ">
-      <MainNav cartLength={cartLength} cart={cart} />
-      <Carousel data-bs-theme="dark" className="home_header_carousel ">
+    <section className="home_header bg-dark">
+      <MainNav
+        cartLength={cartLength}
+        cart={cart}
+      />
+      <Carousel
+        data-bs-theme="dark"
+        className="home_header_carousel">
         {headerImgs.map((img) => {
           return (
             <Carousel.Item
@@ -29,23 +34,20 @@ const Header = ({ cartLength, cart }) => {
                 width="100%"
                 alt={img.title}
                 src={img.img}
+                className="carousel-img"
               />
 
-              <div className="home_header_carousel-caption_container w-50 m-0 m-auto">
-                <Carousel.Caption>
-                  <h1 className="animate__animated animate__fadeIn">
-                    <Image
-                      height="auto"
-                      width="auto"
-                      src={sliderlogo}
-                      alt="logo"
-                      className="px-3 home_header_carousel_logo "
-                    />
-                  </h1>
-                  <Row className="home_header_carousel-cta-container d-flex gap-3 justify-content-center">
+              <div className="home_header_carousel-caption_container m-0 m-auto">
+                <Carousel.Caption className="d-flex align-items-center justify-content-center flex-column ">
+                  <Image
+                    src={sliderlogo}
+                    alt="logo"
+                    className="px-3 home_header_carousel_logo animate__animated animate__fadeIn"
+                  />
+                  <Row className="home_header_carousel-cta-container justify-content-center gap-4">
                     <a
                       href="/PricingPlan"
-                      className="mt-3 btn-dark animate__animated animate__fadeInUp">
+                      className="mt-3 btn-dark animate__animated animate__fadeInUp animate__delay-1s">
                       View More
                       <div className="button-container">
                         <Button
@@ -55,6 +57,7 @@ const Header = ({ cartLength, cart }) => {
                         </Button>
                       </div>
                     </a>
+
                     <a
                       href="/Checkout"
                       className="mt-3 btn-dark animate__animated animate__fadeInUp animate__delay-1s">
